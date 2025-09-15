04e085b542b6d1c275fdee5013cd637a812db227
gutembergmnmb1993 <153691575+gutembergmnmb1993@users.noreply.github.com>
2025-09-15 18:40:37 +0000

    nova mensagem do commit

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,22 @@
-function Home () {
-<<<<<<< HEAD
-return <h1> ola, tudo bem? </h1>;
-=======
-return <h1> Miguel chegou ao mundo antes do tempo, pequeno e frágil, trazendo consigo uma mistura de medo e esperança. Prematuro, cada suspiro dele era uma vitória, e cada olhar, uma promessa de coragem. Os primeiros dias foram difíceis: 20 longos dias de hospital, cercado por máquinas, médicos e enfermeiros que cuidavam dele com todo carinho do mundo.
-
-Mas Miguel era um lutador. Dia após dia, ele mostrava força e determinação, surpreendendo todos com sua vitalidade e garra. E, como se carregasse um pouco de magia no coração, ele venceu cada obstáculo com a tenacidade de quem nasceu para brilhar.
-
-Hoje, Miguel é um menino cheio de saúde, alegria e energia. Seu sorriso ilumina a casa e seu riso contagia todos ao redor. Cada abraço dele é um lembrete de que a vida é preciosa e que a luta vale a pena. Miguel não é apenas um sobrevivente: ele é uma fonte de amor e felicidade, um verdadeiro presente que transforma todos os dias de quem tem a sorte de estar perto dele. </h1>;
->>>>>>> 7d54e7d (mensagem para o miguel)
+function Home() {
+  return (
+    <div>
+      <h1>História do Miguel</h1>
+      <h1>
+        Miguel chegou ao mundo antes do tempo, pequeno e frágil, trazendo consigo
+        uma mistura de medo e esperança. Prematuro, cada suspiro dele era uma vitória,
+        e cada olhar, uma promessa de coragem. Os primeiros dias foram difíceis:
+        20 longos dias de hospital, cercado por máquinas, médicos e enfermeiros que
+        cuidavam dele com todo carinho do mundo.
+      </h1>
+      <h1>
+        Mas Miguel era um lutador. Dia após dia, ele mostrava força e determinação,
+        surpreendendo todos com sua vitalidade e garra. E, como se carregasse um
+        pouco de magia no coração, ele venceu cada obstáculo com a tenacidade
+        de quem nasceu para brilhar.
+      </h1>
+    </div>
+  );
 }
 
 export default Home;
